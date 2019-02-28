72c90c9a6f4553d0f2a7b44464ef353862bbfb10
Radu Tomuleasa <rtomuleasa@eddyfi.com>
2019-02-28 18:30:18 -0500

    Benchmark setup

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Security.Cryptography;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace Benchmarks
+{
+	[ClrJob, CoreJob]
+	public class Benchmark
+	{
+		[Benchmark]
+		public bool AreStringsEqual() =>
+				"This string contains a subtle difference."
+				.Equals(
+				"This string contains a subtle difference?");
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			BenchmarkRunner.Run<Benchmark>();
+		}
+	}
+}
