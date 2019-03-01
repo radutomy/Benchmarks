abb74ed8cad08235baea24230ff6043791da6f78
Radu Tomuleasa <rtomuleasa@eddyfi.com>
2019-02-28 21:17:51 -0500

    span vs array copy

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Security.Cryptography;
@@ -10,14 +11,15 @@ using BenchmarkDotNet.Running;
 namespace Benchmarks
 {
 	[ClrJob, CoreJob]
-	// [InProcessAttribute]
-	public class Benchmark
+	public class ClrVsCore
 	{
 		[Benchmark]
 		public bool AreStringsEqual() =>
 			"This reasonably long short but sweet string could actually contain a subtle difference."
 			.Equals(
 			"This reasonably long short but sweet string could actually contain a subtle difference?");
+
+		// ======================================================================= //
 
 		[Benchmark]
 		public void LoopbackStreamSync()
@@ -51,13 +53,61 @@ namespace Benchmarks
 				}
 			}
 		}
+
+		// ======================================================================= //
+
+		[Benchmark]
+		public void TestBoxing()
+		{
+			ISomeEvilInterface animal = new SomeEvilClass(42);
+			animal.DoSomething();
+		}
 	}
+
+	// ======================================================================= //
+
+	[CoreJob]
+	public class SpanVsArray
+	{
+		[GlobalSetup]
+		public void MakeString() => _text = new string(Enumerable.Repeat('1', 100000).ToArray());
+
+		[Benchmark]
+		public string TestSubstring() => _text.Substring(0, _text.Length / 2);
+		string _text;
+
+		[Benchmark]
+		public ReadOnlySpan<char> TestSlice() => _text.AsSpan().Slice(0, _text.Length / 2);
+	}
+
+	// ======================================================================= //
 
 	class Program
 	{
 		static void Main(string[] args)
 		{
-			BenchmarkRunner.Run<Benchmark>();
+			BenchmarkRunner.Run<ClrVsCore>();
+			BenchmarkRunner.Run<SpanVsArray>();
 		}
 	}
+
+	// ======================================================================== // 
+
+	public struct SomeEvilClass : ISomeEvilInterface
+	{
+		private int _value;
+		public SomeEvilClass(int value)
+		{
+			_value = value;
+		}
+
+		private void DoSomethingEvil() { }
+		void ISomeEvilInterface.DoSomething() => DoSomethingEvil();
+	}
+
+	public interface ISomeEvilInterface
+	{
+		void DoSomething();
+	}
+
 }
